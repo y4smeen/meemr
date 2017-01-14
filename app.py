16b3e3909976a2bc29769e41bbd31f87972aba7e
Yasmeen Roumie <y4smeen@gmail.com>
2017-01-14 15:49:09 -0600

    get meme based on emotion

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+#!flask/bin/python
+from flask import Flask, render_template, jsonify
 import urllib2, json
+from random import randint
 
 app = Flask(__name__)
 
@@ -7,6 +9,13 @@ app = Flask(__name__)
 def index():
 	retun "hey"
 
+@app.route("/memes", methods=['GET'])
+def get_meme_text(emotion):
+	with open('DankMemeStash.json') as data_file:
+		data = json.load(data_file)
+	m_index = randint(0, len(data[emotion])-1)
+	meme = data[emotion][m_index]
+	return meme
 
 if __name__ == "__main__":
 	app.debug = True
